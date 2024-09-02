f1097b470d92bce0ec7c8ebfe4c74685c661e8d0
Muhammad Ahmad Tahir <132302808+ahmaddevcodez@users.noreply.github.com>
2024-09-02 15:14:06 +0500

    build fix

--- src/context/UserContextProvider.jsx
+++ src/context/UserContextProvider.jsx
@@ -1,7 +1,23 @@
 import { useState } from "react";
 import UserContext from "./userContext"; //
 
-const UserContextProvider = ({ children}) => {
+// export const UserContextProvider = ({ children }) => {
+//   const [userData, setUserData] = useState(null);
+
+//   const setUserDataValue = (data) => {
+//     setUserData(data);
+//   };
+
+//   return (
+//     <UserContext.Provider value={{ user: userData, setUser: setUserDataValue }}>
+//       {children}
+//     </UserContext.Provider>
+//   );
+// };
+
+// export default UserContextProvider;
+
+const UserContextProvider = ({ children }) => {
   const [userData, setUserData] = useState(null);
 
   const setUserDataValue = (data) => {
